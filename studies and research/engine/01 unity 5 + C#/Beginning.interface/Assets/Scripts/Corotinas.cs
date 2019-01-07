6f271425e225caf8c383b26b7341f8faee8aa4ec
RafaelMath <rgildo38@gmail.com>
2019-01-06 21:48:45 -0300

    Updating

---
+++ studies and research/engine/01 unity 5 + C#/Beginning.interface/Assets/Scripts/Corotinas.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Corotinas : MonoBehaviour
+{
+    public float segundosDeEspera = 1;
+    void Start()
+    {
+        StartCoroutine(CorotinaTeste1(segundosDeEspera));   
+    }
+    IEnumerator CorotinaTeste (float segundos)
+    {
+        //executa alguns comandos
+        // espera por alguns segundos
+        //volta a executar comandos
+        print("Comecei a executar uma corotina.");
+        yield return new WaitForSeconds(segundos); //espere por 5 segundo depois volta a executar a linha seguinte
+        print("Depois de 5 segundos, volto a executar a função normalmente.");
+
+    }
+    IEnumerator CorotinaTeste1 (float segundos)
+    {
+        print("Comecei a executar uma corotina.");
+        float tempoAtual = 0;
+        while (tempoAtual < segundos)
+        {
+            //passar pro próximo frame
+            tempoAtual += Time.deltaTime; //o tempo que o ultimo frame demorou pra ser executado
+            print("Esperando...");
+            yield return new WaitForEndOfFrame();
+        }
+        print("Depois de " + segundos + "segundos, volto a executar a função normalmente.");
+
+    }
+    // Essa corotina conta até 10 e depois executa outra coisa
+    IEnumerator CorotinaBase ()
+    {
+        yield return new WaitForEndOfFrame();
+        print("Vou contar até 10...");
+        StartCoroutine(ContarAte10());
+        //as linhas só serão executadas quando as corotinas acima forem executadas
+        int soma = 10 + 4;
+        print("O resultado da soma: " + soma);
+    }
+    IEnumerator ContarAte10()
+    {
+        for(int i = 1; i<= 10; i++)
+        {
+            print(i);
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
---
+++ studies and research/engine/01 unity 5 + C#/Beginning.interface/Assets/Scripts/tutorial.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorial : MonoBehaviour
+{
+    public int meuInteiro;
+    public float meuFlutuante;
+    public string meuTexto;
+    public bool meuBooleano;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
